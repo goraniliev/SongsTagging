154c5d84dd99a7c1f9d70c67ab00007b1bff69f6
KireP <kire_m.j@hotmail.com>
2015-12-17 16:17:33 +0100

    Crawling track tags independently

--- IKT/src/crawler/TrackInfoCrawler.java
+++ IKT/src/crawler/TrackInfoCrawler.java
@@ -6,15 +6,20 @@ import java.io.InputStreamReader;
 import java.net.URLEncoder;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import utility.Tag;
+import utility.Tags;
+import utility.TopTags;
 import utility.Track;
 import utility.TrackWrapper;
 
 import com.google.gson.Gson;
+
+import filters.FilterFactory;
 
 public class TrackInfoCrawler {
 
@@ -22,6 +27,12 @@ public class TrackInfoCrawler {
 		
 		String artist = "Bon Jovi";
 		String songName = "It's My Life";
+		allSongInformations(artist, songName);
+	}
+	
+	public static void allSongInformations(String artist, String songName) 
+			throws ClientProtocolException, IOException {
+		// For song info crawling
 		String query = "http://ws.audioscrobbler.com/2.0/?method=track.getInfo"
 				+ "&artist=" 
 				+ URLEncoder.encode(artist, "utf-8") 
@@ -29,17 +40,8 @@ public class TrackInfoCrawler {
 				+ URLEncoder.encode(songName, "utf-8")
 				+ "&api_key=dd499d6c71a646deb54725b96bf4baa8&format=json";
 		
-		Track track = getTrackInfo(query);
-		
+		Track track = getSongInfo(query);
 		if (track != null) {
-			if (track.getToptags() != null) {
-				System.out.println("Tags:");
-				for (Tag tag : track.getToptags().getTag()) {
-					System.out.println(tag.getName());
-				}
-				System.out.println("=====================");
-			}
-			
 			if (track.getListeners() != null) {
 				System.out.println("Listeners: " + track.getListeners());
 				System.out.println("=====================");	
@@ -52,12 +54,29 @@ public class TrackInfoCrawler {
 			
 			if (track.getWiki() != null) {
 				System.out.println("Published on: " + track.getWiki().getPublishedDate());	
+				System.out.println("=====================");
 			}	
+		}
+		
+		// For tags crawling
+		query = "http://ws.audioscrobbler.com/2.0/?method=track.getToptags"
+				+ "&artist=" 
+				+ URLEncoder.encode(artist, "utf-8") 
+				+ "&track=" 
+				+ URLEncoder.encode(songName, "utf-8")
+				+ "&api_key=dd499d6c71a646deb54725b96bf4baa8&format=json";
+		
+		Tags tags = getSongTags(query);
+		if (tags != null && tags.getTag() != null) { 
+			Tag[] filteredTags = FilterFactory.getTagFilter().filterTags(tags.getTag());
+			System.out.println("Tags:");
+			for (Tag tag : filteredTags) {
+				System.out.println(tag);
+			}
 		}
 	}
 
-	public static Track getTrackInfo(String query) throws IllegalStateException, IOException {
-		
+	public static Track getSongInfo(String query) throws IllegalStateException, IOException {
 		HttpClient client = new DefaultHttpClient();
 		HttpGet getMethod = new HttpGet(query);
 		HttpResponse response = client.execute(getMethod);
@@ -77,5 +96,28 @@ public class TrackInfoCrawler {
 		}
 
 		return track.getTrack();
+	}
+	
+
+	public static Tags getSongTags(String query) throws ClientProtocolException, IOException {
+		HttpClient client = new DefaultHttpClient();
+		HttpGet getMethod = new HttpGet(query);
+		HttpResponse response = client.execute(getMethod);
+
+		if (response.getStatusLine().getStatusCode() != 200) {
+			return null;
+		}
+
+		InputStream is = response.getEntity().getContent();
+		if (is == null) {
+			return null;
+		}
+		
+		TopTags topTags = new Gson().fromJson(new InputStreamReader(is), TopTags.class);
+		if (topTags == null) {
+			return null;
+		}
+		
+		return topTags.getToptags();
 	}
 }
---
+++ IKT/src/utility/TopTags.java
@@ -0,0 +1,19 @@
+package utility;
+
+public class TopTags {
+
+	private Tags toptags;
+	
+	public TopTags(Tags toptags) {
+		this.toptags = toptags;
+	}
+
+	public Tags getToptags() {
+		return toptags;
+	}
+
+	public void setToptags(Tags toptags) {
+		this.toptags = toptags;
+	}
+	
+}
