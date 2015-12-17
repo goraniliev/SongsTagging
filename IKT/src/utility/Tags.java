8a50791f0d1be23a33d4fe3fbc7cb2f678f33963
KireP <kire_m.j@hotmail.com>
2015-12-17 03:09:20 +0100

    Check

---
+++ IKT/src/utility/Tags.java
@@ -0,0 +1,19 @@
+package utility;
+
+public class Tags {
+
+	private Tag[] tag;
+	
+	public Tags(Tag[] tag) {
+		this.tag = tag;
+	}
+
+	public Tag[] getTag() {
+		return tag;
+	}
+
+	public void setTag(Tag[] tag) {
+		this.tag = tag;
+	}
+	
+}
