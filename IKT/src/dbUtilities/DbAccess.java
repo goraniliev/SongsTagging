bd2c06a52f36baae571e0372c59b06e83eb9c637
Your Name <goraniliev93@yahoo.com>
2015-12-19 18:38:47 +0100

    DbAccess added as a base class

---
+++ IKT/src/dbUtilities/DbAccess.java
@@ -0,0 +1,9 @@
+package dbUtilities;
+
+import java.sql.Connection;
+
+public class DbAccess {
+	protected String pass = "goranpass";
+	protected String userDB = "root";
+	protected Connection conn =  null;
+}
