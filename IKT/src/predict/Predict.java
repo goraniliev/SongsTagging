6862a719eaae1cbb8ebf4bfaba58ad7b7a66bc66
Your Name <goraniliev93@yahoo.com>
2015-12-20 00:22:49 +0100

    Refactoring

--- IKT/src/predict/Predict.java
+++ IKT/src/predict/Predict.java
@@ -6,9 +6,12 @@ import java.util.List;
 import dbUtilities.Selects;
 
 public class Predict {
-	double predictHotnessForTags(List<String> tags)
+	public static double predictHotnessForTags(List<String> tags)
 			throws InstantiationException, IllegalAccessException,
 			ClassNotFoundException, SQLException {
+		/**
+		 * Returns predicted hotness for given list of tags which belongs to some new song.
+		 */
 		Selects selects = new Selects();
 		return selects.predictedHotnessForGivenTags(tags);
 	}
