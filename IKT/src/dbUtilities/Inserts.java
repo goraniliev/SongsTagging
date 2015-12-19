667609bac5c54ab7d87b3c905a15c428842fd2d4
Your Name <goraniliev93@yahoo.com>
2015-12-20 00:21:18 +0100

    Refactoring

--- IKT/src/dbUtilities/Inserts.java
+++ IKT/src/dbUtilities/Inserts.java
@@ -1,10 +1,12 @@
 package dbUtilities;
 
 import java.sql.CallableStatement;
-import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
+
+import predict.Predict;
 
 public class Inserts extends DbAccess{
 
@@ -15,6 +17,9 @@ public class Inserts extends DbAccess{
 	}
 
 	int insertTag(String tagName) throws SQLException {
+		/**
+		 * Inserts a new tag.
+		 */
 		String sql = "{call insert_tag(?)}";
 		CallableStatement st = conn.prepareCall(sql);
 		st.setString("name", tagName);
@@ -31,6 +36,10 @@ public class Inserts extends DbAccess{
 	}
 	
 	int insertSong(String name, String artist, long numPlays, long numListeners, double coeff/*, String releaseDate*/) throws SQLException {
+		/**
+		 * Inserts a new song. This method is used when filling the database in the start, with old songs which already have known number
+		 * of plays and listeners.
+		 */
 		int last_id = 0;
 		
 		String sql = "{call insert_song(?, ?, ?, ?, ?)}";
@@ -52,7 +61,28 @@ public class Inserts extends DbAccess{
 		return last_id;
 	}
 	
+	public void insertNewSong(String name, String artist, List<String> tags) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
+		/**
+		 * Inserts a new song. The number of plays and listeners is 0. The hotness is calculated based on the predict function which takes as argument
+		 * list of song's tag.
+		 */
+		
+		double hotness = Predict.predictHotnessForTags(tags);
+		System.out.printf("Predicted hotness for %s is %f\n", name, hotness);
+		String sql = "{call insert_new_song(?, ?, ?)}";
+		CallableStatement st = conn.prepareCall(sql);
+		st.setString("songname", name);
+		st.setString("artist", artist);
+		st.setDouble("hotness", hotness);
+//		st.setString("releaseDate", releaseDate);
+		st.execute();
+		
+	}
+	
 	boolean isNewTag(String tagName) throws SQLException {
+		/**
+		 * Returns true if there is already such tag in the Tag table in database.
+		 */
 		String sql = "{call is_tag_present(?)}";
 		CallableStatement st = conn.prepareCall(sql);
 		st.setString("tagname", tagName);
@@ -61,7 +91,7 @@ public class Inserts extends DbAccess{
 		int count = 0;
 		
 		ResultSet resultSet = st.getResultSet();
-//		
+		
 		while(resultSet.next()) {
 			count = resultSet.getInt("cnt");
 		}
@@ -70,6 +100,10 @@ public class Inserts extends DbAccess{
 	}
 	
 	int getTagIdByName(String tagName) throws SQLException {
+		/**
+		 * Returns tag id for given tagName.
+		 */
+		
 		String sql = "{call get_id_tag_by_name(?)}";
 		CallableStatement st = conn.prepareCall(sql);
 		st.setString("name", tagName);
@@ -78,7 +112,7 @@ public class Inserts extends DbAccess{
 		int id = -1;
 		
 		ResultSet resultSet = st.getResultSet();
-//		
+		
 		while(resultSet.next()) {
 			id = resultSet.getInt("id");
 		}
@@ -87,6 +121,9 @@ public class Inserts extends DbAccess{
 	}
 	
 	void insertSongTag(int idSong, int idTag) throws SQLException {
+		/**
+		 * Inserts new row in the SongTag table in database.
+		 */
 		String sql = "{call insert_song_tag(?, ?)}";
 		CallableStatement st = conn.prepareCall(sql);
 		st.setInt("idsong", idSong);
@@ -94,9 +131,10 @@ public class Inserts extends DbAccess{
 		st.execute();
 	}
 	
-
-	
 	void close() throws SQLException {
+		/**
+		 * Close connection to database.
+		 */
 		conn.close();
 	}
 }
