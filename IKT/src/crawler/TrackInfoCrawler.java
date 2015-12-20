a05a2f4c4f5654c332aad2d21c1983161fad370d
KireP <kire_m.j@hotmail.com>
2015-12-20 20:43:17 +0100

    Threaded crawling

--- IKT/src/crawler/TrackInfoCrawler.java
+++ IKT/src/crawler/TrackInfoCrawler.java
@@ -7,7 +7,9 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
@@ -27,11 +29,114 @@ import com.google.gson.Gson;
 import filters.FilterFactory;
 
 public class TrackInfoCrawler {
+	
+	private static final int numThreads = 100;
+	
+	static class CrawlerThread extends Thread {
+		
+		private List<TrackAllInfo> tracks;
+		private List<String[]> trackParameters;
+		private String threadName;
+		
+		public CrawlerThread(List<String[]> trackParameters, String threadName) {
+			this.trackParameters = trackParameters;
+			this.tracks = new ArrayList<>();
+			this.threadName = threadName;
+		}
+		
+		@Override
+		public void run() {
+			int counter = 0;
+			for (String[] pair : this.trackParameters) {
+				TrackAllInfo trackInfo = null;
+				try {
+					counter++;
+					trackInfo = allTrackInformations(pair[0], pair[1]);
+				} catch (ClientProtocolException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} finally {
+					if (trackInfo != null) {
+						this.tracks.add(trackInfo);
+					}	
+					if (counter % 5 == 0) {
+						System.out.println(threadName + " - Tracks crawled: " + counter);	
+					}
+				}
+			}
+		}
+		
+		public List<TrackAllInfo> getTracks() {
+			return this.tracks;
+		}
+	}
 
-	public static void main(String[] args) throws IllegalStateException, IOException {
+	public static void main(String[] args) throws IllegalStateException, IOException, InterruptedException {
+		/*
 		List<TrackAllInfo> tracks = crawl("unique_tracks.txt", 0, 3);
-		System.out.println("Number of tracks:" + tracks.size());
+		System.out.println("Number of tracks: " + tracks.size());
 		System.out.println(tracks);
+		*/
+		
+		List<TrackAllInfo> tracks = crawl_threaded("unique_tracks.txt", 0, 499);
+		System.out.println("Number of tracks: " + tracks.size());
+		System.out.println(tracks);
+	}
+	
+	public static List<TrackAllInfo> crawl_threaded(String fileName, int start, int end) 
+			throws IOException, InterruptedException {
+		
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
+		for (int i = 0; i < start; i++) {
+			br.readLine();
+		}
+		String line = "";
+		List<String[]> trackParameters = new ArrayList<>();
+		int counter = 0;
+		while ((line = br.readLine()) != null) {
+			if (counter == (end - start + 1)) {
+				break;
+			}
+			counter++;
+			
+			String[] parts = line.split("<SEP>");
+			trackParameters.add(new String[] { parts[2], parts[3] });
+		}
+		br.close();
+		
+		Set<CrawlerThread> threads = new HashSet<>();
+		int tracksCount = trackParameters.size() / numThreads;
+		for (int i = 0; i < numThreads; i++) {
+			if (i == numThreads - 1) {
+				threads.add(new CrawlerThread(
+						trackParameters.subList(i * tracksCount, trackParameters.size()),
+						"Thread " + i
+						)
+				);
+			} else {
+				threads.add(new CrawlerThread(
+					trackParameters.subList(i * tracksCount, i * tracksCount + tracksCount),
+					"Thread " + i
+					)
+				);
+			}
+		}
+		
+		for (CrawlerThread thread : threads) {
+			thread.start();
+		}
+		
+		for (CrawlerThread thread : threads) {
+			thread.join();
+		}
+		
+		List<TrackAllInfo> result = new ArrayList<>();
+		for (CrawlerThread thread : threads) {
+			result.addAll(thread.getTracks());
+		}
+		
+		return result;
 	}
 	
 	public static List<TrackAllInfo> crawl(String fileName, int start, int end) throws IOException {
@@ -146,7 +251,7 @@ public class TrackInfoCrawler {
 		HttpClient client = new DefaultHttpClient();
 		HttpGet getMethod = new HttpGet(query);
 		HttpResponse response = client.execute(getMethod);
-
+		
 		if (response.getStatusLine().getStatusCode() != 200) {
 			return null;
 		}
