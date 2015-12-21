de305384b646465bf59256f6b5d9d1d694ce0a85
Your Name <goraniliev93@yahoo.com>
2015-12-21 03:01:50 +0100

    throws added

--- IKT/src/examples/FillDataInTheEmptyDatabase.java
+++ IKT/src/examples/FillDataInTheEmptyDatabase.java
@@ -8,7 +8,7 @@ import dbUtilities.FillDatabase;
 //	2. Driver class
 public class FillDataInTheEmptyDatabase {
 	// Example how to fill the database from the file.
-	public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException, IOException, SQLException {
-		FillDatabase.insertTracksFromAPIToDatabase("unique_tracks.txt", 10000);
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException, IOException, SQLException, InterruptedException {
+		FillDatabase.insertTracksFromAPIToDatabase("unique_tracks.txt", 10);
 	}
 }
