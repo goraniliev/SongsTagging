538ccdaacfc892428e3cd93f86ac0f5d335d7504
Aleksandar Kuzmanoski <kuzmanoski.aleksandar@students.finki.ukim.mk>
2015-12-21 01:29:37 +0100

    predictionDataAccess

---
+++ IKT/src/prediction/helper/PredictionDirectDataAccess.java
@@ -0,0 +1,19 @@
+package prediction.helper;
+
+import java.util.Map;
+
+public class PredictionDirectDataAccess implements PredictionDataAccess {
+
+	@Override
+	public Map<String, Integer> getTags() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<Integer, Double> getTracks(Integer tag) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
